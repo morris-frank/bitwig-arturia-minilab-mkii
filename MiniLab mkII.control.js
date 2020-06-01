c19744c9999fcb2bd75ad5f5e5709cb3e1189a1e
Maurice Frank <maurice.frank@posteo.de>
2020-06-01 22:15:18 +0200

    updt

--- MiniLab mkII.control.js
+++ MiniLab mkII.control.js
@@ -6,19 +6,53 @@ host.defineMidiPorts(1, 1);
 
 host.addDeviceNameBasedDiscoveryPair(["Arturia MiniLab mkII MIDI 1"], ["Arturia MiniLab mkII MIDI 1"]);
 
+var STATUS_KNOB = 176;
+
+var KNOB_1_CLCK = 113;
+var KNOB_9_CLICK = 115;
+
+var KNOBS_LEFT = [112, 74, 71, 76, 114, 18, 19, 16];
+var KNOBS_RIGHT = [77, 93, 73, 75, 17, 91, 79, 72];
+
+var KNOBS = KNOBS_LEFT.concat(KNOBS_RIGHT);
+
 function init() {
    transport = host.createTransport();
 
    var midiPort = host.getMidiInPort(0);
-   midiPort.setMidiCallback(onMidi0);
+   midiPort.setMidiCallback(onMidi);
    midiPort.setSysexCallback(onSysex0);
+
+   cTrack = host.createCursorTrack(3, 0);
+   deviceCursor = cTrack.createCursorDevice();
+   controlPageCursor = deviceCursor.createCursorRemoteControlsPage(8);
+
+   uControl = host.createUserControls(8);
+   for (var i = 0; i < 8; i++)
+   {
+       uControl.getControl(i).setLabel("CC " + KNOBS_RIGHT[i])
+   }
 
    println("> init finished");
 }
 
 // Called when a short MIDI message is received on MIDI input port 0.
-function onMidi0(status, data1, data2) {
-   printMidi(status, data1, data2);
+function onMidi(status, key, value) {
+   if (status == STATUS_KNOB) {
+      var knob = KNOBS.indexOf(key);
+
+      if (knob > 7){
+         knob -= 8;
+         controlPageCursor.getParameter(knob).setImmediately(value / 128);
+         println('L ' + knob + ': ' + value);
+      } else if (knob >= 0) {
+         println('RIGHT ' + knob + ': ' + value);
+      } else {
+         if (key == KNOB_9_CLICK) {
+            resetRemoteControl();
+         }
+      }
+   }
 }
 
 // Called when a MIDI sysex message is received on MIDI input port 0.
@@ -45,6 +79,16 @@ function onSysex0(data) {
 
 function flush() {
 }
+
+
+function resetRemoteControl()
+{
+    for (var i = 0; i < 8; i++)
+    {
+        controlPageCursor.getParameter(i).reset();
+    }
+}
+
 
 function exit() {
    println("> exit");
